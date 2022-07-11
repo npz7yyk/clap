6e47ffe36a7a6614451f66a5afe46377bd0129d6
Yikang Yue <1127168778@qq.com>
2022-07-11 10:26:35 +0800

    bare runable version

--- src/branch/branch_unit.v
+++ src/branch/branch_unit.v
@@ -29,30 +29,30 @@ module branch_unit #(
     input wire                    rstn,
 
     input wire                    ifVld,            // whether this unit works
-    input wire [HASH_WIDTH - 1:0] ifPC,             // instruction to predict
+    input wire [ADDR_WIDTH - 1:0] ifPC,             // instruction to predict
 
     input wire                    idVld,            // whether signal from id is valid
-    input wire [HASH_WIDTH - 1:0] idPC,             // pc of instruction pair being decoded
-    input wire [HASH_WIDTH - 1:0] idPCTar1,         // branch target of instruction1
-    input wire [HASH_WIDTH - 1:0] idPCTar2,         // branch target of instruction2
+    input wire [ADDR_WIDTH - 1:0] idPC,             // pc of instruction pair being decoded
+    input wire [ADDR_WIDTH - 1:0] idPCTar1,         // branch target of instruction1
+    input wire [ADDR_WIDTH - 1:0] idPCTar2,         // branch target of instruction2
     input wire              [1:0] idType1,          // type of instruction1
     input wire              [1:0] idType2,          // type of instruction2
 
     input wire                    exVld,            // whether signal from ex is valid
-    input wire [HASH_WIDTH - 1:0] exPC,             // pc of instruction under execution
-    input wire [HASH_WIDTH - 1:0] exPCTar,          // branch target of this instruction
+    input wire [ADDR_WIDTH - 1:0] exPC,             // pc of instruction under execution
+    input wire [ADDR_WIDTH - 1:0] exPCTar,          // branch target of this instruction
     input wire              [1:0] exType,           // instruction type
     input wire                    exBranch,         // whether this instruction branches
     input wire                    exWrong,          // ex pc wrongly predicted
 
-    output wire [HASH_WIDTH - 1:0] pdPC,            // pc predicted
+    output wire [ADDR_WIDTH - 1:0] pdPC,            // pc predicted
     output wire                    pdBranch,        // whether a branch is predicted
     output wire                    pdReason         // if branch, which inst causes this
 );
     // start of instruction fact part
     wire [1:0] erFactSel, exFactSel, ifFactSel;
     wire [ADDR_WIDTH - 1:0] ifFactData1, ifFactData2;
-    fact #(
+    data #(
         .ADDR_WIDTH (ADDR_WIDTH),
         .HASH_DEPTH (HASH_DEPTH),
         .HASH_WIDTH (HASH_WIDTH)
@@ -67,18 +67,16 @@ module branch_unit #(
         .idPCTarLower   (idPCTar1),
         .idPCTarUpper   (idPCTar2),
 
-        .erSel          (erFactSel),
-        .erLower        (erFactLower),
-        .erUpper        (erFactUpper),
+        .idExist        (idFactExist),
+        .idInsert       (idFactInsert),
+        .idIsPair       (idFactIsPair),
 
         .exPC           (exPC),
         .exExist        (exFactExist),
-        .exSel          (exFactSel),
 
         .ifPC           (ifPC),
         .ifExistLower   (ifFactExist1),
         .ifExistUpper   (ifFactExist2),
-        .ifSel          (ifFactSel),
         .ifDataLower    (ifFactData1),
         .ifDataUpper    (ifFactData2)
     );
@@ -88,6 +86,14 @@ module branch_unit #(
     parameter PAST_DEPTH = HASH_DEPTH + 1;
     parameter PAST_WIDTH = 10;
 
+    assign erFactLower = idFactIsPair ? 
+        1'b1 : 
+        idFactInsert ? 1'b0 : idType1[1] & idType1[0];
+    assign erFactUpper = idFactIsPair ? 
+        1'b1 : 
+        idFactInsert ? 1'b0 : idType2[1] & idType2[0];
+    assign exBack = exPCTar[ADDR_WIDTH - 1:2] < exPC[ADDR_WIDTH - 1:2];
+
     /**
      * fact log data structure (from high to low):
      * name     bits    function
@@ -96,7 +102,7 @@ module branch_unit #(
      *                  each case has 2 bits
      */
     wire [PAST_WIDTH - 1:0] ifPastPara1, ifPastPara2;
-    past #(
+    para #(
         .ADDR_WIDTH (ADDR_WIDTH),
         .HASH_DEPTH (PAST_DEPTH),
         .PARA_WIDTH (PAST_WIDTH)
@@ -104,21 +110,18 @@ module branch_unit #(
         .clk            (clk),
         .rstn           (rstn),
 
-        .erEn           (idVld),
-        .erSel          (erFactSel),
+        .erEn           (idVld & ~idFactExist),
         .erPC           (idPC),
         .erLower        (erFactLower),
         .erUpper        (erFactUpper),
 
         .bdEn           (exVld & exFactExist),
-        .bdSel          (exFactSel),
         .bdPC           (exPC),
-        .bdPCTar        (exPCTar),
+        .bdBack         (exBack),
         .bdType         (exType),
         .bdBranch       (exBranch),
 
         .ifPC           (ifPC),
-        .ifSel          (ifFactSel),
         .ifVldLower     (ifPastVld1),
         .ifVldUpper     (ifPastVld2),
         .ifParaLower    (ifPastPara1),
