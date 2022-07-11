cb1908f021d1a12fe6b38cc55a0fd1a6dc1569ad
MaZirui2001 <454434635@qq.com>
2022-07-11 17:53:02 +0800

    update cache

--- src/cache/cache_mem.v
+++ src/cache/cache_mem.v
@@ -4,18 +4,18 @@
 
 //TODO: write first
 module cache_memory #(
-  parameter NB_COL    = 64,                        // Specify number of columns (number of bytes)
-  parameter COL_WIDTH = 8,                        // Specify column width (byte width, typically 8 or 9)
-  parameter RAM_DEPTH = 64,                      // Specify RAM depth (number of entries)
-  parameter INIT_FILE = ""                        // Specify name/location of RAM initialization file if using one (leave blank if not)
+  parameter NB_COL    = 64,                         // Specify number of columns (number of bytes)
+  parameter COL_WIDTH = 8,                          // Specify column width (byte width, typically 8 or 9)
+  parameter RAM_DEPTH = 64,                         // Specify RAM depth (number of entries)
+  parameter INIT_FILE = ""                          // Specify name/location of RAM initialization file if using one (leave blank if not)
 ) (
-  input [clogb2(RAM_DEPTH-1)-1:0] addra,  // Write address bus, width determined from RAM_DEPTH
-  input [clogb2(RAM_DEPTH-1)-1:0] addrb,  // Read address bus, width determined from RAM_DEPTH
-  input [(NB_COL*COL_WIDTH)-1:0] dina,  // RAM input data\
+  input [clogb2(RAM_DEPTH-1)-1:0] addra,            // Write address bus, width determined from RAM_DEPTH
+  input [clogb2(RAM_DEPTH-1)-1:0] addrb,            // Read address bus, width determined from RAM_DEPTH
+  input [(NB_COL*COL_WIDTH)-1:0] dina,              // RAM input data\
   input ena,
-  input clka,                           // Clock
-  input [NB_COL-1:0] wea,               // Byte-write enable
-  output [(NB_COL*COL_WIDTH)-1:0] doutb // RAM output data
+  input clka,                                       // Clock
+  input [NB_COL-1:0] wea,                           // Byte-write enable
+  output [(NB_COL*COL_WIDTH)-1:0] doutb             // RAM output data
 );
 
   reg [(NB_COL*COL_WIDTH)-1:0] BRAM [RAM_DEPTH-1:0];
