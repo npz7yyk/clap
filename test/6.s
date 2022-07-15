b5e6730676bd7a2146c0e5c4d9eab0d49737c595
WCIofQMandRA <zichen350@gmail.com>
2022-07-15 17:59:03 +0800

    add test 6

---
+++ test/6.s
@@ -0,0 +1,38 @@
+# branch
+.globl _start 
+.type _start, @function
+.text
+.align 2
+
+_start:
+    addi.w $r1,$r0,1000
+    addi.w $r5,$r0,1989
+.L1:
+    addi.w $r2,$r2,5
+    addi.w $r1,$r1,-1
+    bnez $r1,.L1
+    addi.w $r31,$r2,0   # r31 = 5000
+    addi.w $r2,$r2,1000
+    addi.w $r3,$r0,1
+    addi.w $r1,$r0,1
+.L2:
+    beq $r3,$r2,.L3
+    mul.w $r1,$r3,$r1
+    mod.w $r1,$r1,$r5
+    addi.w $r3,$r3,1
+    b .L2
+.L3:
+    add.w $r30,$r3,$r0  # r30 = 1000! mod 1989
+    addi.w $r1,$r0,1000
+    addi.w $r2,$r0,1
+    addi.w $r3,$r0,0
+.L6:
+    bge $r2,$r1,.L4
+    andi $r4,$r2,1
+    beqz $r4,.L5
+    add.w $r3,$r2,$r3
+.L5:
+    addi.w $r2,$r2,1
+    b .L6
+.L4:
+    add.w $r29,$r3,$r0  # r29 = 1+3+..+999 = 250000
