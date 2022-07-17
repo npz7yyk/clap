232f6b0b523e4a5f3b83469c8d8ac54eec937980
MaZirui2001 <454434635@qq.com>
2022-07-17 15:55:09 +0800

    modified:   src/cache~main_FSM_i.v 	modified:   src/cache~ret_buf_i.v

--- src/cache~ret_buf_i.v
+++ src/cache~ret_buf_i.v
@@ -5,11 +5,16 @@ module ret_buf_i(
     output reg [511:0] mem_din,
     output reg fill_finish
     );
+    reg ret_valid_pos;
+    always @(posedge clk) begin
+        ret_vlaid_pos <= ret_valid;
+    end
+
     always @(posedge clk) begin
         if(ret_valid)begin
             mem_din = (mem_din >> 32) | {r_data_AXI, 480'b0};
         end
-        if(ret_last) fill_finish = 1;
+        if(!ret_valid_pos & ret_valid) fill_finish = 1;
         else fill_finish = 0;
     end
 endmodule
